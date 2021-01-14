0f0c9dd7688b8e1edc84dd427acae28803ec03c3
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-01-14 17:23:39 +0100

    Save only last sub results

--- Milestone_3/2_src/UC_model.py
+++ Milestone_3/2_src/UC_model.py
@@ -463,10 +463,11 @@ for stor_level_max in stor_levels_max:
     #---------------------------------------------------------------------------
 
     if output:
-        # with open(
-        #     os.path.join(path, f'results_sub_{stor_level_max}.json'),
-        #     'w') as outfile:
-        #     json.dump(results_sub, outfile)
+        # Save only the sub results of the last iteration.
+        with open(
+            os.path.join(path, f'results_sub_{stor_level_max}.json'),
+            'w') as outfile:
+            json.dump(results_sub[iteration], outfile)
 
         with open(
             os.path.join(path, f'results_master_{stor_level_max}.json'),
