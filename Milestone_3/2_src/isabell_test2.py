943e133c96c1e84c5bc7b95076f4e7a7396c1929
Isa <juno_ivf@web.de>
2021-01-15 12:53:13 +0100

    concurrent test files

--- Milestone_3/2_src/isabell_test2.py
+++ Milestone_3/2_src/isabell_test2.py
@@ -2,12 +2,21 @@ import concurrent.futures
 import time
 
 start = time.perf_counter()
-secs = [5, 4, 3, 2, 1]
 
-for i in secs:
-    print(f'Sleeping {i} second(s) ...')
-    time.sleep(i)
-    print (f'Done Sleeping...{i}')
-        
-finish = time.perf_counter()
-print(f'Finished in {round(finish-start, 2)} second(s)')
+
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
+    return f'Done Sleeping...{seconds}'
+
+if __name__ == '__main__':
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        secs = [5, 4, 3, 2, 1]
+        results = executor.map(do_something, secs)
+
+        # for result in results:
+        #     print(result)
+
+    finish = time.perf_counter()
+
+    print(f'Finished in {round(finish-start, 2)} second(s)')
