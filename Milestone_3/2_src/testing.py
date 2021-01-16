9befc05637eaa20bcb3e0af731c79efd4ee2cda2
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-01-16 17:41:22 +0100

    Include multiprocessing

--- Milestone_3/2_src/testing.py
+++ Milestone_3/2_src/testing.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 from pathlib import Path
 import pandas as pd
+import concurrent.futures
 
 from modeling_helper.utilities import *
 from modeling_helper.printing import *
@@ -15,15 +16,15 @@ opt = SolverFactory('gurobi')
 
 current_path = Path.cwd()
 
-# **************************************************************************
+# ******************************************************************************
 # Model Options
-# **************************************************************************
+# ******************************************************************************
 
 # Model options are defined in the file 'model_options.py'.
 
-# **************************************************************************
+# ******************************************************************************
 # Parameters
-# **************************************************************************
+# ******************************************************************************
 
 # Seet for randomness
 seed = 17
@@ -39,9 +40,9 @@ stor_level_max = 4
 
 # All other parameteres are defined in the file 'parameters.py'
 
-# **************************************************************************
+# ******************************************************************************
 # Testing
-# **************************************************************************
+# ******************************************************************************
 
 # Create dictionary for objective values.
 objective_values = {approach: [] for approach in APPROACHES}
@@ -85,156 +86,172 @@ for approach in APPROACHES:
         break
     print(f'Solving for all {test_size} test samples')
 
-    for i, sample in enumerate(TEST_SAMPLES):
+    # **************************************************************************
+    # Model
+    # **************************************************************************
 
-        print_status(i, test_size)
+    model = pyo.ConcreteModel()
 
-        # ******************************************************************
-        # Model
-        # ******************************************************************
+    # **************************************************************************
+    # Sets
+    # **************************************************************************
 
-        model = pyo.ConcreteModel()
+    # Hour sets
+    model.H = pyo.RangeSet(1,len(HOURS)-1)
+    model.H_all = pyo.RangeSet(0, len(HOURS)-1)
 
-        # ******************************************************************
-        # Sets
-        # ******************************************************************
+    # **************************************************************************
+    # Parameter
+    # **************************************************************************
 
-        # Hour sets
-        model.H = pyo.RangeSet(1,len(HOURS)-1)
-        model.H_all = pyo.RangeSet(0, len(HOURS)-1)
+    # Create a parameter for the load values.
+    model.load_values = pyo.Param(model.H_all, mutable=True)
 
-        # ******************************************************************
-        # Variables
-        # ******************************************************************
+    # **************************************************************************
+    # Variables
+    # **************************************************************************
 
-        # Fixed model problem variables
-        model.u = pyo.Var(model.H_all)
-        model.p1 = pyo.Var(model.H_all)
-        for h in model.H_all:
-            model.u[h].fix(parameter['u'][str(h)])
-            model.p1[h].fix(parameter['p1'][str(h)])
+    # Fixed model problem variables
+    model.u = pyo.Var(model.H_all)
+    model.p1 = pyo.Var(model.H_all)
+    for h in model.H_all:
+        model.u[h].fix(parameter['u'][str(h)])
+        model.p1[h].fix(parameter['p1'][str(h)])
 
-        # Electricity produced by generator
-        model.pg = pyo.Var(model.H_all, within=pyo.NonNegativeReals)
-        # Initialization for p1
-        model.pg[0].fix(0)
+    # Electricity produced by generator
+    model.pg = pyo.Var(model.H_all, within=pyo.NonNegativeReals)
+    # Initialization for p1
+    model.pg[0].fix(0)
 
-        # Electrictiy bought from retailer
-        model.p2 = pyo.Var(model.H_all, within=pyo.NonNegativeReals)
-        # Initialization for p1
-        model.p2[0].fix(0)
+    # Electrictiy bought from retailer
+    model.p2 = pyo.Var(model.H_all, within=pyo.NonNegativeReals)
+    # Initialization for p1
+    model.p2[0].fix(0)
 
-        if esr:
-            # Net injection by storage
-            model.stor_net_i = pyo.Var(model.H, bounds=(-p_w_max, p_i_max))
-            # Initialization for net injection
-            model.stor_net_i[0].fix(0)
+    if esr:
+        # Net injection by storage
+        model.stor_net_i = pyo.Var(model.H, bounds=(-p_w_max, p_i_max))
+        # Initialization for net injection
+        model.stor_net_i[0].fix(0)
 
-            # Storage level
-            model.stor_level = pyo.Var(
-                model.H_all,
-                within=pyo.NonNegativeReals,
-                bounds=(0, stor_level_max)
+        # Storage level
+        model.stor_level = pyo.Var(
+            model.H_all,
+            within=pyo.NonNegativeReals,
+            bounds=(0, stor_level_max)
+        )
+        # Initialization for net injection
+        model.stor_level[0].fix(0)
+
+    # **************************************************************************
+    # Objective function
+    # **************************************************************************
+
+    model.OBJ = pyo.Objective(
+        expr=sum(
+            c1*model.u[h] + l1*model.p1[h]
+            + c2*model.pg[h] + l2*model.p2[h] for h in model.H
+        )
+    )
+
+    # **************************************************************************
+    # Constraints
+    # **************************************************************************
+
+    if up_down_time:
+        # Minimum uptime constraint
+        def min_uptime(model, H):
+            # Apply minimum uptime constraint.
+            # The end value of the range function needs to be increased
+            # to be included.
+            V = list(range(H, min([H-1 + uptime, len(HOURS)-1]) + 1))
+            # For the last hour, the ouput of the range function is 0
+            # because range(24,24). To include hour 24 into the list,
+            # check for length and put hour 24 into V.
+            if len(V) == 0:
+                V = [H]
+            # Return the sum of all hours in V to apply the constraint
+            # for all hours in V.
+            return sum(
+                model.u[H] - model.u[H-1] for v in V
+                ) <= sum(model.u[v] for v in V)
+        model.min_uptime = pyo.Constraint(model.H, rule=min_uptime)
+
+        # Minimum downtime constraint
+        def min_downtime(model, H):
+            # Apply minimum downtime constraint.
+            # The end value of the range function needs to be increased
+            # to be included.
+            V = list(range(H, min([H-1 + downtime, len(HOURS)-1]) + 1))
+            # For the last hour, the ouput of the range function is 0
+            # because range(24,24). To include hour 24 into the list,
+            # check for length and put hour 24 into V.
+            if len(V) == 0:
+                V = [H]
+            # Return the sum of all hours in V to apply the constraint
+            # for all hours in V.
+            return sum(
+                model.u[H-1] - model.u[H] for v in V
+                ) <= sum(1 - model.u[v] for v in V)
+        model.min_downtime = pyo.Constraint(model.H, rule=min_downtime)
+
+    if esr:
+        # Load must be covered by production, purchasing electrictiy or
+        # by net injection of storage system.
+        def con_load(model, H):
+            return (model.pg[H] + model.p1[H] + model.p2[H]
+                + model.stor_net_i[H]) >= model.load_values[H]
+        model.con_load = pyo.Constraint(model.H, rule=con_load)
+    else:
+        # Load must be covered by production or purchasing electrictiy.
+        def con_load(model, H):
+            return (model.pg[H] + model.p1[H]
+                    + model.p2[H]) >=  model.load_values[H]
+        model.con_load = pyo.Constraint(model.H, rule=con_load)
+
+    # maximum capacity of generator
+    def con_max(model, H):
+        return model.pg[H] <= pmax*model.u[H]
+    model.con_max = pyo.Constraint(model.H, rule=con_max)
+
+    if ramping:
+        # Ramping constraint of generator
+        def con_ramping(model, H):
+            return (
+                -ramping_constraint,
+                model.pg[H] - model.pg[H-1],
+                ramping_constraint
             )
-            # Initialization for net injection
-            model.stor_level[0].fix(0)
+        model.con_ramping = pyo.Constraint(model.H, rule=con_ramping)
 
-        # ******************************************************************
-        # Objective function
-        # ******************************************************************
-
-        model.OBJ = pyo.Objective(
-            expr=sum(
-                c1*model.u[h] + l1*model.p1[h]
-                + c2*model.pg[h] + l2*model.p2[h] for h in model.H
+    if esr:
+        # Storage Balance
+        def stor_balance(model, H):
+            return model.stor_level[H] == (
+                model.stor_level[H-1] - model.stor_net_i[H]
             )
+        model.stor_balance = pyo.Constraint(model.H, rule=stor_balance)
+
+    # Use concurrent package to enable multiprocessing to solve test samples in
+    # paralell.
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = executor.map(
+            solve_sample,
+            TEST_SAMPLES,
+            list(range(len(TEST_SAMPLES))),
+            [len(TEST_SAMPLES)] * len(TEST_SAMPLES),
+            [model] * len(TEST_SAMPLES),
+            [opt] * len(TEST_SAMPLES),
+            [True] * len(TEST_SAMPLES)
         )
 
-        # ******************************************************************
-        # Constraints
-        # ******************************************************************
-
-        if up_down_time:
-            # Minimum uptime constraint
-            def min_uptime(model, H):
-                # Apply minimum uptime constraint.
-                # The end value of the range function needs to be increased
-                # to be included.
-                V = list(range(H, min([H-1 + uptime, len(HOURS)-1]) + 1))
-                # For the last hour, the ouput of the range function is 0
-                # because range(24,24). To include hour 24 into the list,
-                # check for length and put hour 24 into V.
-                if len(V) == 0:
-                    V = [H]
-                # Return the sum of all hours in V to apply the constraint
-                # for all hours in V.
-                return sum(
-                    model.u[H] - model.u[H-1] for v in V
-                    ) <= sum(model.u[v] for v in V)
-            model.min_uptime = pyo.Constraint(model.H, rule=min_uptime)
-
-            # Minimum downtime constraint
-            def min_downtime(model, H):
-                # Apply minimum downtime constraint.
-                # The end value of the range function needs to be increased
-                # to be included.
-                V = list(range(H, min([H-1 + downtime, len(HOURS)-1]) + 1))
-                # For the last hour, the ouput of the range function is 0
-                # because range(24,24). To include hour 24 into the list,
-                # check for length and put hour 24 into V.
-                if len(V) == 0:
-                    V = [H]
-                # Return the sum of all hours in V to apply the constraint
-                # for all hours in V.
-                return sum(
-                    model.u[H-1] - model.u[H] for v in V
-                    ) <= sum(1 - model.u[v] for v in V)
-            model.min_downtime = pyo.Constraint(model.H, rule=min_downtime)
-
-        # Take first random vector from samples
-        pl = sample
-        if esr:
-            # Load must be covered by production, purchasing electrictiy or
-            # by net injection of storage system.
-            def con_load(model, H):
-                return (model.pg[H] + model.p1[H] + model.p2[H]
-                    + model.stor_net_i[H]) >= pl[H]
-            model.con_load = pyo.Constraint(model.H, rule=con_load)
-        else:
-            # Load must be covered by production or purchasing electrictiy.
-            def con_load(model, H):
-                return model.pg[H] + model.p1[H] + model.p2[H] >= pl[H]
-            model.con_load = pyo.Constraint(model.H, rule=con_load)
-
-        # maximum capacity of generator
-        def con_max(model, H):
-            return model.pg[H] <= pmax*model.u[H]
-        model.con_max = pyo.Constraint(model.H, rule=con_max)
-
-        if ramping:
-            # Ramping constraint of generator
-            def con_ramping(model, H):
-                return (
-                    -ramping_constraint,
-                    model.pg[H] - model.pg[H-1],
-                    ramping_constraint
-                )
-            model.con_ramping = pyo.Constraint(model.H, rule=con_ramping)
-
-        if esr:
-            # Storage Balance
-            def stor_balance(model, H):
-                return model.stor_level[H] == (
-                    model.stor_level[H-1] - model.stor_net_i[H]
-                )
-            model.stor_balance = pyo.Constraint(model.H, rule=stor_balance)
-
-        # solve model
-        solve_model(opt, model)
-
-        results_dic[approach][i] = get_results(model)
-
-        objective_values[approach].append(pyo.value(model.OBJ))
+    # Results are stored in a map object and have to be unpacked
+    # into a dict.
+    for i, result in enumerate(results):
+        results_dic[approach][i] = result
+        objective_values[approach].append(
+            results_dic[approach][i]['objective_value']
+        )
 
     # **************************************************************************
     # Export
