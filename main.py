d9b6c0220417296f82ae1d606a3a36f18f63316a
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-03-12 23:51:03 +0100

    Add exemplary usage of framework

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from seqsuc import SequentialSampling, Parameter
+from seqsuc import SequentialSampling, Parameter, LShapeMethod
 
-
+# Create all relevant parameters
 params = Parameter()
 
-seq = SequentialSampling(params, 'MC', False, 'SRP')
+# Run an exemplary l shape method
+lsh = LShapeMethod(
+    params=params,
+    sample_size=100,
+    seed=12,
+    sampling_method='MC',
+    multiprocessing=True,
+    progress_info=True
+)
+lsh.solve_model()
 
+# Run exemplary sequential sampling method
+seq = SequentialSampling(
+    params=params,
+    sampling_method='AV',
+    estimator_method='A2RP',
+    multiprocessing=True,
+    output=True
+)
 seq.run_seq_sampling()
